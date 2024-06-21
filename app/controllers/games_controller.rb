ed1a1a8b07294df33191f74d8c91cac6dfc04a9e
YassirMaghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-06-21 14:49:00 +0100

    debug the GamesController class

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,11 @@
+require "open-uri"
+
 class GamesController < ApplicationController
   VOWELS = %w(A E I O U Y)
 
   def new
-    @letters = Array.new(5) {VOWELS.sample}
-    @letters += Array.new(5) {(("A".."Z").to_a - VOWELS).sample}
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
     @letters.shuffle!
   end
 
@@ -15,15 +17,14 @@ class GamesController < ApplicationController
   end
 
   private
+
   def included?(word, letters)
-    word.chars.all?{|letter| word.count(letter) <= letters.count(letter)}
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
   def english_word?(word)
     response = URI.open("https://dictionary.lewagon.com/#{word}")
     json = JSON.parse(response.read)
     json['found']
-    rescue OpenURI::HTTPError
-      false
   end
 end
